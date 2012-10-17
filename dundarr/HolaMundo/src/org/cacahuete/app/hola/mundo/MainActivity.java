021921a89f345864da47aa0e8e792c8f8c09e571
Nicolas Blanco Skrlec <milipunto@gmail.com>
2012-10-17 20:26:13 +0200

    Hola mundo variante tu movil va a explotar
    Solo funciona con android 4, con la 2.2 no iba

--- dundarr/HolaMundo/src/org/cacahuete/app/hola/mundo/MainActivity.java
+++ dundarr/HolaMundo/src/org/cacahuete/app/hola/mundo/MainActivity.java
@@ -3,6 +3,8 @@ package org.cacahuete.app.hola.mundo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
+import android.graphics.Typeface;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,18 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+     // personalizamos la fuente del texto autor, para que use English.ttf,
+        // que se encuentra en la carpeta Assets.
+        TextView customFont = (TextView)findViewById(R.id.linea3);
+        Typeface font = Typeface.createFromAsset (getAssets (), "FEASFBRG.TTF");
+        customFont.setTypeface(font);
+        
+     // personalizamos la fuente del texto autor, para que use English.ttf,
+        // que se encuentra en la carpeta Assets.
+        TextView customFont2 = (TextView)findViewById(R.id.linea5);
+        Typeface font2 = Typeface.createFromAsset (getAssets (), "ROCKFONT.TTF");
+        customFont2.setTypeface(font2);
     }
 
     @Override
