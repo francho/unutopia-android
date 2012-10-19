fbc04b56ab98c0ec190184148ed8e8cafe8c7ae0
Nicolas Blanco Skrlec <milipunto@gmail.com>
2012-10-19 19:38:12 +0200

    Primer intento, funciona pero es feo todavia
    Falta la parte de poner bonitas las pantallas justamente, por si os
    sirviera el codigo de poner un splash y saltar a la pantalla about ...

---
+++ dundarr/feedreader-splash/src/org/cacahuete/app/feedreader/splash/AboutScreenActivity.java
@@ -0,0 +1,20 @@
+package org.cacahuete.app.feedreader.splash;
+
+import android.os.Bundle;
+import android.app.Activity;
+//import android.view.Menu;
+
+
+/**
+* About screen activity
+*
+*/
+public class AboutScreenActivity extends Activity {
+	
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.about);
+	}		 
+}
