21711b4465411567646c6b6682b3b9fccb11cd2b
merybere <marietabc@wanadoo.es>
2012-10-22 03:15:08 +0200

    Ejercicio RSS Hangout 2

---
+++ merybere/Ejercicio RSS/src/com/merybere/apps/ejercicio_rss/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.merybere.apps.ejercicio_rss;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_splash, menu);
+        return true;
+    }
+}
