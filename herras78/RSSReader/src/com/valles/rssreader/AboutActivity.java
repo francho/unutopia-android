19ca1ac4184c1d9bc42865b3fdc6a45704555e5a
Alvaro valles <alvaro.valles@moventos.com>
2012-10-23 14:44:59 +0200

    Theme ActivityDialogNoTitle y animacion cargando.. da paso a boton

---
+++ herras78/RSSReader/src/com/valles/rssreader/AboutActivity.java
@@ -0,0 +1,40 @@
+package com.valles.rssreader;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class AboutActivity extends Activity{
+
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+		final TextView Aceptar = (TextView) findViewById(R.id.btn_aceptar);
+		Aceptar.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {           	
+            	Salir();            	
+            }
+        });
+	}
+	
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			Salir();
+           return true;
+        }
+        return false;
+    }
+	
+	public void Salir(){
+		Intent intent = new Intent(AboutActivity.this, ArticleListActivity.class);     
+        startActivity(intent); 
+	}
+
+}
---
+++ herras78/RSSReader/src/com/valles/rssreader/ArticleListActivity.java
@@ -0,0 +1,61 @@
+package com.valles.rssreader;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class ArticleListActivity extends Activity {
+	 
+	
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.article_list);
+        
+        final TextView TxtTitulo = (TextView) findViewById(R.id.lbltitulo);
+        final Typeface font1 = Typeface.createFromAsset(getAssets(),"Last Ninja.ttf");
+        TxtTitulo.setTypeface(font1);
+        
+        final ImageView acercade = (ImageView) findViewById(R.id.acercade);
+        acercade.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {           	
+            	Intent intent = new Intent(ArticleListActivity.this, AboutActivity.class);     
+                startActivity(intent); 
+            	//PopAcerca();            	
+            }
+        });
+    }
+	
+	/*public void PopAcerca(){
+
+		final AlertDialog.Builder acerca = new AlertDialog.Builder(this);
+		final View vista = LayoutInflater.from(this).inflate(R.layout.popacercade,(ViewGroup) findViewById(R.id.dialogacercade));
+		final TextView Titulo = (TextView) vista.findViewById(R.id.lblpoptit);
+		final Typeface font1 = Typeface.createFromAsset(getAssets(),"Last Ninja.ttf");
+		Titulo.setTypeface(font1);
+		
+		acerca.setIcon(R.drawable.ayudab);
+		acerca.setTitle("Acerca de...");
+		acerca.setView(vista);
+		acerca.setPositiveButton("Aceptar",new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			} 
+		});
+		acerca.create();
+		acerca.show();
+	}*/
+}
+
