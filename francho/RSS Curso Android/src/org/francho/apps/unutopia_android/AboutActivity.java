c1c672fb94b2589160ea2a0a30de1122e4a88995
Francho <lab@francho.org>
2012-10-24 18:30:29 +0200

    Paso 2 de la app

---
+++ francho/RSS Curso Android/src/org/francho/apps/unutopia_android/AboutActivity.java
@@ -0,0 +1,83 @@
+package org.francho.apps.unutopia_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class AboutActivity extends Activity {
+
+	private CountDownTimer timer;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_about);
+
+		setVersionNumber();
+		enableLinks();
+	}
+
+	private void setVersionNumber() {
+		final TextView versionView = (TextView) findViewById(R.id.about_version);
+		if (null == versionView) {
+			return;
+		}
+
+		try {
+			final PackageInfo pkgInfo = getPackageManager().getPackageInfo(
+					getPackageName(), 0);
+			final String format = getString(R.string.about_version);
+			final String versionTxt = String.format(format,
+					pkgInfo.versionName, pkgInfo.versionCode);
+			versionView.setText(versionTxt);
+		} catch (NameNotFoundException e) {
+			versionView.setText(R.string.about_unknown_version);
+		}
+	}
+
+	private void enableLinks() {
+		setAboutLink();
+
+		enableLinksInText(R.id.about_intro, getString(R.string.about_text));
+		enableLinksInText(R.id.about_legal, getString(R.string.about_license));
+	}
+
+	private void setAboutLink() {
+		final TextView text = (TextView) findViewById(R.id.about_subtitle);
+		if (null == text) {
+			return;
+		}
+
+		text.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("http://francho.org/about"));
+				startActivity(intent);
+			}
+		});
+
+	}
+
+	private void enableLinksInText(int resId, String source) {
+		final TextView view = (TextView) findViewById(resId);
+		if (null == view) {
+			return;
+		}
+		
+		view.setFocusable(true);
+		view.setLinksClickable(true);
+		Linkify.addLinks(view, Linkify.ALL);
+	}
+}
--- francho/RSS Curso Android/src/org/francho/apps/unutopia_android/SplashActivity.java
+++ francho/RSS Curso Android/src/org/francho/apps/unutopia_android/SplashActivity.java
@@ -1,23 +1,72 @@
 package org.francho.apps.unutopia_android;
 
-import android.os.Bundle;
+import org.francho.apps.unutopia_android.app.AppIntent;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
-import android.widget.TextView;
+import android.view.View.OnClickListener;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends Activity implements OnClickListener {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        
-    }
+	private CountDownTimer timer = null;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_splash, menu);
-        return true;
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+		View rootView = findViewById(R.id.splash_root);
+		rootView.setOnClickListener(this);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		timer = new NextActivityTimer();
+		timer.start();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		
+		if(null != timer) {
+			timer.cancel();
+			timer=null;
+		}
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		startNextActivity();
+	};
+	
+	private void startNextActivity() {
+		final Intent intent = AppIntent.getArticleListIntent();
+		startActivity(intent);
+	}
+
+	class NextActivityTimer extends CountDownTimer {
+
+		public NextActivityTimer() {
+			super(10000, 10000);
+		}
+
+		@Override
+		public void onTick(long millisUntilFinished) {
+			// Do nothing
+		}
+
+		@Override
+		public void onFinish() {
+			startNextActivity();
+		}
+	}
+
+	
+
 }
