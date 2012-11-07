6e5f88da825e23442268d430c6641d80c8fe62bb
gothalo <gothalo@gmail.com>
2012-11-07 17:30:08 +0100

    se accede a la base de datos via un content provider. por ahora el content provider solo permite query y insert

--- gothalo/LectoRSS/src/cat/foixench/apps/lectorss/db/RssContract.java
+++ gothalo/LectoRSS/src/cat/foixench/apps/lectorss/db/RssContract.java
@@ -1,5 +1,6 @@
 package cat.foixench.apps.lectorss.db;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 
@@ -11,6 +12,9 @@ import android.provider.BaseColumns;
 public class RssContract {
 	
 	public static final String DB_NAME = "rss.db";
+	public static final String AUTHORITY = "cat.foixench.apps.lectorss";
+	public static final String FEEDS_PATH = "feeds";
+	
 	
 	/**
 	 * definimos un constructor privado para evitar que se creen instancias de esta clase. 
@@ -31,6 +35,13 @@ public class RssContract {
 		public static final String COLUMN_DESCRIPTION = "description";
 		public static final String COLUMN_AUTHOR = "author";
 		
+		public static Uri getUri() {
+			return Uri.parse ("content://" + AUTHORITY + "/" + FEEDS_PATH);
+		}
+
+		public static Uri getUri(long id) {
+			return Uri.parse("content://" + AUTHORITY + "/" + FEEDS_PATH + "/" + id);
+		}
 	}
 
 }
