a391fda19f3b7eb7d91563b76e1b192922448cb4
Nicolas Blanco Skrlec <milipunto@gmail.com>
2012-11-07 20:47:41 +0100

    completado content provider

---
+++ dundarr/ContentProviderPruebas/src/org/cacahuete/app/contentproviderpruebas/data/MyDbHelper.java
@@ -0,0 +1,34 @@
+package org.cacahuete.app.contentproviderpruebas.data;
+
+import org.cacahuete.app.contentproviderpruebas.data.MembersContract.UsersTable;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDbHelper extends SQLiteOpenHelper {
+	
+	private static final int DATABASE_VERSION = 1;
+
+	public MyDbHelper(Context context) {
+		super(context, MembersContract.DB_NAME, null, DATABASE_VERSION );
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE " + UsersTable.TABLE_NAME + "("
+					+ UsersTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ UsersTable.USERNAME + " STRING,"
+					+ UsersTable.EMAIL + " STRING"
+					+")"
+				);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
