22bcb17d482f27fdb05f64fd14c883b16fe9a6bb
Nicolas Blanco Skrlec <milipunto@gmail.com>
2012-11-21 22:04:14 +0100

    barra de carga funcional

--- dundarr/FeedReader/src/org/cacahuete/app/feedreader/db/RssIntentService.java
+++ dundarr/FeedReader/src/org/cacahuete/app/feedreader/db/RssIntentService.java
@@ -1,12 +1,16 @@
 package org.cacahuete.app.feedreader.db;
 
 
+import java.net.URL;
+import java.util.ArrayList;
+
+import nl.matshofman.saxrssreader.RssItem;
+
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.ResultReceiver;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
+
 
 public class RssIntentService extends IntentService {
 
@@ -21,20 +25,40 @@ public class RssIntentService extends IntentService {
 	
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		Log.d(TAG, "onHandleIntent");
+		Log.d(TAG, "iniciando IntentService");
 		
 		resultReceiver = (ResultReceiver) intent.getParcelableExtra("EXTRA_MYRESULTRECEIVER");
 		
-		if(resultReceiver != null) {
-			resultReceiver.send(1, null);
-		}
 		
 		RssReload reloadTask = new RssReload();
-		reloadTask.reloadArticles(this.getContentResolver());
+		reloadTask.borrarArticulos(getContentResolver());
 		
+		reloadTask.loadFeeds(getContentResolver());
 		if(resultReceiver != null) {
-			resultReceiver.send(0, null);
+			resultReceiver.send(10, null);
 		}
+		Float porcentaje_por_feed=(float) (90/reloadTask.getNumberOfFeeds());
+		Float porcentaje_acumulado=Float.valueOf(10);
+		Log.d(TAG,"Porcentaje por feed es "+porcentaje_por_feed.toString());
+		Log.d(TAG,"Progreso es "+porcentaje_acumulado.toString()+".Hay "+reloadTask.getNumberOfFeeds()+" feeds");
+		for(URL url : reloadTask.getFeedList()) {
+			
+			ArrayList<RssItem> lista_articulos=reloadTask.reloadArticlesForFeed(url);
+	//		Log.d(TAG,"Progreso es "+porcentaje_acumulado.toString()+".Hay "+lista_articulos.size()+" articulos en feed");
+			Float porcentaje_por_articulo=porcentaje_por_feed/lista_articulos.size();
+			Log.d(TAG,"El porcentaje por articulo es "+porcentaje_por_articulo.toString());
+			for(RssItem rssItem : lista_articulos) {
+				reloadTask.insertArticle(getContentResolver(), rssItem);
+				porcentaje_acumulado=porcentaje_acumulado+porcentaje_por_articulo;
+				Log.d(TAG,"Progreso es "+porcentaje_acumulado.toString()+", insertando articulo");
+				Integer progreso=Math.round(porcentaje_acumulado);
+				if(resultReceiver != null) {
+					resultReceiver.send(progreso, null);
+				}
+			}
+		}
+		
+
 	}
 
 	@Override
--- dundarr/FeedReader/src/org/cacahuete/app/feedreader/db/RssReload.java
+++ dundarr/FeedReader/src/org/cacahuete/app/feedreader/db/RssReload.java
@@ -1,8 +1,13 @@
 package org.cacahuete.app.feedreader.db;
 
-import java.net.MalformedURLException;
+
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
 
 import nl.matshofman.saxrssreader.RssFeed;
 import nl.matshofman.saxrssreader.RssItem;
@@ -22,11 +27,9 @@ public class RssReload {
 
 	
 	private static final String TAG = "RSS reload";
-
-	public void reloadArticles(ContentResolver rss) {
-		Log.d(TAG, "reload start");
-		
-		
+	private ArrayList<URL> feed_list;
+	
+	public void loadFeeds(ContentResolver rss) {
 		String sortOrder = null;
 		String[] selectionArgs = null;
 		String selection = null;
@@ -37,50 +40,167 @@ public class RssReload {
 		
 		Cursor cursor=rss.query(uri, projection, selection, selectionArgs, sortOrder);
 		Log.d(TAG,"cursor tiene "+cursor.getColumnCount());
-		Uri uri_insert_articles=ArticlesTable.getUri();
 		
+		this.feed_list=new ArrayList<URL>();
 		try {
 			while (cursor.moveToNext()) {
-				URL url=new URL(cursor.getString(cursor.getColumnIndex("url")));
+				URL url=new URL(cursor.getString(cursor.getColumnIndex(FeedsTable.URL)));
+				this.feed_list.add(url);
 				Log.d(TAG,"nueva url es "+url.toString());
-				try {
-					RssFeed feed = RssReader.read(url);
-					//Log.d(TAG,"FEED ES "+feed.toString());
-				
-					ArrayList<RssItem> rssItems = feed.getRssItems();
-					Log.d(TAG,"feed tiene "+rssItems.size()+" elementos");				
-					for(RssItem rssItem : rssItems) {
-						ContentValues values = new ContentValues();
-						values.put(ArticlesTable.TITLE,rssItem.getTitle());
-						values.put(ArticlesTable.CONTENT,rssItem.getContent());
-						values.put(ArticlesTable.LINK,rssItem.getLink());
-						values.put(ArticlesTable.DESCRIPTION,rssItem.getDescription());
-						//values.put(ArticlesTable.PUBDATE,rssItem.getPubDate().toString());
-						rss.insert(uri_insert_articles, values);
-//						Log.d("RSS Reader ", rssItem.getTitle());
-//						Log.d("RSS Reader ", rssItem.getContent());
-//						Log.d("RSS Reader ", rssItem.getDescription());
-//						Log.d("RSS Reader ", rssItem.getLink());
-//						Log.d("RSS Reader ", rssItem.getFeed().toString());
-					}//for
-				} catch (Exception e) {
-					Log.d(TAG,"Problemas con internet");
-					
-				}
-				
-			}//while
-			Log.d(TAG, "reload stop at "+System.currentTimeMillis()*1000+" segs");
+			}
 		} catch (Exception e) {
-			Log.d(TAG,"Problemas con la bd");
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				Log.d(TAG,"Problemas con la bd");
+				cursor.close();
+				e.printStackTrace();
+		}//try
+		cursor.close();
+	}
+	
+	public Integer getNumberOfFeeds() {
+		Integer num=this.feed_list.size();
+		return num;
+		
+	}
+	public ArrayList<URL> getFeedList() {
+		
+		return this.feed_list;
+		
+	}
+	
+	public void borrarArticulos(ContentResolver rss) {   
+		//vaciamos tabla de articulos
+		rss.delete(ArticlesTable.getUri(), null, null);
+	}
+
+	public ArrayList<RssItem> reloadArticlesForFeed(URL url) {
+		RssFeed feed=null;
+		ArrayList<RssItem> rssItems=null;
+	
+		try {
+			feed = RssReader.read(url);
+			 rssItems = feed.getRssItems();
+			}
+		catch (Exception e) {}
+		
+		return rssItems;
+}
+	
+	public void insertArticle(ContentResolver rss,RssItem rssItem) {
+		Uri uri_insert_articles=ArticlesTable.getUri();
+		
+		ContentValues values = new ContentValues();
+		values.put(ArticlesTable.TS_DATE,Long.valueOf(rssItem.getPubDate().getTime()));
+		Date fecha=new Date(rssItem.getPubDate().getTime());
+		//String fecha_formateada =new SimpleDateFormat("dd.MM.yyyy HH:mm").format(fecha);
+		String fecha_formateada =new SimpleDateFormat("dd.MM.yyyy").format(fecha);
+		values.put(ArticlesTable.PUBDATE,fecha_formateada);
+		//Log.d(TAG,values.toString());
+		
+//		try {
+//			values.put(ArticlesTable.TITLE,URLEncoder.encode(rssItem.getTitle(), "UTF-8"));
+//		} catch (UnsupportedEncodingException e) {
+//			values.put(ArticlesTable.TITLE,rssItem.getTitle());
+//		}
+//		try {
+//			values.put(ArticlesTable.CONTENT,URLEncoder.encode(rssItem.getContent(), "UTF-8"));
+//		} catch (UnsupportedEncodingException e) {
+//			values.put(ArticlesTable.CONTENT,rssItem.getContent());
+//		}
+//		try {
+//			values.put(ArticlesTable.LINK,URLEncoder.encode(rssItem.getLink(), "UTF-8"));
+//		} catch (UnsupportedEncodingException e) {
+//			values.put(ArticlesTable.LINK,rssItem.getLink());
+//		}
+//		try {
+//			values.put(ArticlesTable.DESCRIPTION,URLEncoder.encode(rssItem.getDescription(), "UTF-8"));
+//		} catch (UnsupportedEncodingException e) {
+//			values.put(ArticlesTable.DESCRIPTION,rssItem.getDescription());
+//		}
+		
+		try {
+			values.put(ArticlesTable.TITLE,URLDecoder.decode(rssItem.getTitle(), "UTF-8"));
+		} catch (Exception e) {
+			values.put(ArticlesTable.TITLE,rssItem.getTitle());
+		}
+		try {
+			values.put(ArticlesTable.CONTENT,URLDecoder.decode(rssItem.getContent(), "UTF-8"));
+		} catch (Exception e) {
+			values.put(ArticlesTable.CONTENT,rssItem.getContent());
+		}
+		try {
+			values.put(ArticlesTable.LINK,URLDecoder.decode(rssItem.getLink(), "UTF-8"));
+		} catch (Exception e) {
+			values.put(ArticlesTable.LINK,rssItem.getLink());
+		}
+		try {
+			values.put(ArticlesTable.DESCRIPTION,URLDecoder.decode(rssItem.getDescription(), "UTF-8"));
+		} catch (Exception e) {
+			values.put(ArticlesTable.DESCRIPTION,rssItem.getDescription());
 		}
 		
-		cursor.close();
 		
+		//Log.d(TAG,"Vamos a mostrar values e insertar");
 		
-		Log.d(TAG, "hardwordk stop");
+		rss.insert(uri_insert_articles, values);
+		
 	}
+	
+//	public void reloadArticles(ContentResolver rss) {
+//		Log.d(TAG, "RssReload start");
+//		Uri uri_insert_articles=ArticlesTable.getUri();
+//		
+//		
+//		//vaciamos tabla de articulos
+//		rss.delete(ArticlesTable.getUri(), null, null);
+//		
+//		Log.d(TAG,"Terminada recogida de feeds, comenzamos con descarga de articles");
+//		try {
+//			for(URL url : this.feed_list) {
+//				RssFeed feed = RssReader.read(url);
+//				//Log.d(TAG,"FEED ES "+feed.toString());
+//			
+//				ArrayList<RssItem> rssItems = feed.getRssItems();
+//				Log.d(TAG,"feed tiene "+rssItems.size()+" elementos");				
+//				for(RssItem rssItem : rssItems) {
+//					Log.d(TAG,"Comienza el for");
+//					ContentValues values = new ContentValues();
+//					values.put(ArticlesTable.TS_DATE,Long.valueOf(rssItem.getPubDate().getTime()));
+//					Date fecha=new Date(rssItem.getPubDate().getTime());
+//					//String fecha_formateada =new SimpleDateFormat("dd.MM.yyyy HH:mm").format(fecha);
+//					String fecha_formateada =new SimpleDateFormat("dd.MM.yyyy").format(fecha);
+//					values.put(ArticlesTable.PUBDATE,fecha_formateada);
+//					//Log.d(TAG,values.toString());
+//					
+//					values.put(ArticlesTable.TITLE,rssItem.getTitle());
+//					values.put(ArticlesTable.CONTENT,rssItem.getContent());
+//					values.put(ArticlesTable.LINK,rssItem.getLink());
+//					values.put(ArticlesTable.DESCRIPTION,rssItem.getDescription());
+//					
+//					
+//					//Log.d(TAG,"Vamos a mostrar values e insertar");
+//					
+//					rss.insert(uri_insert_articles, values);
+////					Log.d("RSS Reader ", rssItem.getTitle());
+////					Log.d("RSS Reader ", rssItem.getContent());
+////					Log.d("RSS Reader ", rssItem.getDescription());
+////					Log.d("RSS Reader ", rssItem.getLink());
+////					Log.d("RSS Reader ", rssItem.getFeed().toString());
+//				}//for
+//			}
+//			} catch (Exception e) {
+//					Log.d(TAG,"Problemas con internet");
+//					
+//				}
+//				
+//	
+//			//Log.d(TAG, "reload stop at "+System.currentTimeMillis()*1000+" segs");
+//
+//		
+//		
+//		
+//		
+//		Log.d(TAG, "RssReload stop");
+//	}
 	
 	
 
